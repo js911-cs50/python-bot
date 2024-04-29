05917d8860b7b9ccbc79f78c720e6e3460b1cfad
js911-cs50 <jsampathu911@gmail.com>
2024-04-29 13:25:43 -0400

    pushed

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  port = int(os.environ.get('PORT', 8080))  # Use environment variable or default to 8080
+  app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
-    
+
+if __name__ == '__main__':
+    keep_alive()
